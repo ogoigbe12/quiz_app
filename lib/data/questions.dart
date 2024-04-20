e9fb5b139ecab0db41eb6f8fed5de6b621e63c75
Ogoigbe1 <ogoigbe12@gmail.com>
2024-04-20 20:45:11 +0100

    fixed

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -58,62 +58,82 @@ const questions = [
   QuizQuestion(
     'What is Flutter?',
     [
-      'A mobile app development framework'
-          'A new programming language'
-          'An operating system'
-          'A type of bird'
+      'A mobile app development framework',
+      'A new programming language',
+      'An operating system',
+      'A type of bird',
     ],
   ),
-  QuizQuestion('Which programming language is primarily used in Flutter?', [
-    'Dart'
-        'Java'
-        'Python'
-        'C++'
-  ]),
   QuizQuestion(
-      "What does the 'hot reload' feature in Flutter allow developers to do?", [
-    'Instantly see code changes reflected in the app'
-        'Compile the app to native code'
-        'Test the app on multiple devices simultaneously'
-        'Create animations'
-  ]),
+    'Which programming language is primarily used in Flutter?',
+    [
+      'Dart',
+      'Java',
+      'Python',
+      'C++',
+    ],
+  ),
   QuizQuestion(
-      'Which widget in Flutter is used to create a row of items with a fixed height?',
-      [
-        'ListView'
-            'Column'
-            'GridView'
-            'SizedBox'
-      ]),
-  QuizQuestion('What does the MaterialApp widget provide in a Flutter app?', [
-    'Basic app structure and navigation'
-        'Icons and images'
-        'State management'
-        'Database connectivity'
-  ]),
-  QuizQuestion('What does the Scaffold widget provide in a Flutter app?', [
-    'Basic app layout structure like app bars and drawers'
-        'Animation effects'
-        'Data storage'
-        'Network requests'
-  ]),
-  QuizQuestion('What is the purpose of the setState() method in Flutter?', [
-    "To update the UI when the app's state changes"
-        "To define a new stateful widget"
-        "To create a new screen"
-        "To handle user inputs"
-  ]),
+    "What does the 'hot reload' feature in Flutter allow developers to do?",
+    [
+      'Instantly see code changes reflected in the app',
+      'Compile the app to native code',
+      'Test the app on multiple devices simultaneously',
+      'Create animations',
+    ],
+  ),
   QuizQuestion(
-      'Which Flutter command is used to create a new Flutter project?', [
-    'flutter create'
-        'flutter build'
-        'flutter run'
-        'flutter init'
-  ]),
-  QuizQuestion("What does the 'widget' term refer to in Flutter?", [
-    'Immutable UI elements'
-        'User interactions'
-        'Network requests'
-        'Data models'
-  ]),
+    'Which widget in Flutter is used to create a row of items with a fixed height?',
+    [
+      'ListView',
+      'Column',
+      'GridView',
+      'SizedBox',
+    ],
+  ),
+  QuizQuestion(
+    'What does the MaterialApp widget provide in a Flutter app?',
+    [
+      'Basic app structure and navigation',
+      'Icons and images',
+      'State management',
+      'Database connectivity',
+    ],
+  ),
+  QuizQuestion(
+    'What does the Scaffold widget provide in a Flutter app?',
+    [
+      'Basic app layout structure like app bars and drawers',
+      'Animation effects',
+      'Data storage',
+      'Network requests',
+    ],
+  ),
+  QuizQuestion(
+    'What is the purpose of the setState() method in Flutter?',
+    [
+      "To update the UI when the app's state changes",
+      "To define a new stateful widget",
+      "To create a new screen",
+      "To handle user inputs",
+    ],
+  ),
+  QuizQuestion(
+    'Which Flutter command is used to create a new Flutter project?',
+    [
+      'flutter create',
+      'flutter build',
+      'flutter run',
+      'flutter init',
+    ],
+  ),
+  QuizQuestion(
+    "What does the 'widget' term refer to in Flutter?",
+    [
+      'Immutable UI elements',
+      'User interactions',
+      'Network requests',
+      'Data models',
+    ],
+  ),
 ];
